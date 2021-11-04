61074bce91e5dd7c946b3464a12248e754c47201
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-04 14:51:48 +0300

    Initial commit

---
+++ CoreDataExperience/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExperience
+//
+//  Created by Yaroslav Monastyrev on 04.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
